ceb09259235e9de6a1016363465c762f9c677ec1
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-31 03:34:29 +0000

    Aug 31, 2025, 3:34 AM

--- testquestion1.js
+++ testquestion1.js
@@ -2,7 +2,7 @@ function setup(){
     createCanvass(600, 400);
     background(220);
     circle(mouseX, mouseY, 200);
-    circle.gravity('dynamic');
+    circle.collider('dynamic');
 }
 function draw(){
     background(220);
