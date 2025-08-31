7f31bae0be9dbe7b387b4c4d996cd75e169ea148
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-31 03:36:05 +0000

    Aug 31, 2025, 3:36 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,12 @@
-
+function setup(){
+    createCanvass(600, 400);
+    background(220);
+    circle(mouseX, mouseY, 200);
+    circle.collider('dynamic');
+}
+function draw(){
+    background(220);
+    if (mousepresses()){
+        circle()
+    }
+}
