5471c35a18974e389c7d353a9d3a3bc5b58863ca
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-31 03:31:11 +0000

    Aug 31, 2025, 3:31 AM

--- testquestion1.js
+++ testquestion1.js
@@ -6,6 +6,6 @@ function draw(){
     background(220);
     if (mousepresses()){
         circle(mouseX, mouseY, 200);
-        circle.gravity('dynamic')
+        circle.gravity('dynamic');
     }
 }
