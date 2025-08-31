6cee03420449aa2b8ac11d28f7841eb5cf078c11
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-31 03:59:22 +0000

    Aug 31, 2025, 3:59 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,10 +1,10 @@
 function setup(){
     createCanvass(600, 400);
     background(220);
-}
-function draw(){
     new Sprite = ball;
     ball.gravity.Y = ('dynamic');
+}
+function draw(){
     if (mousepresses()){
         ball(mouseX, mouseY);
     }
